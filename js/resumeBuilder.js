d90ad9022de8461700de09b410fa26ae30224c85
Awesome <awesome@idexx.com>
2017-02-01 08:07:12 -0500

    resumeBuilder update & index2-3 add for test

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,16 @@
-$("main").append("Travis");
+console.log("sup?");
+var firstName = "Travis";
+var lastName = "Kroot";
+var name = firstName + " " + lastName;
+var role = "Junior Developer";
+console.log(name);
+var emailIdexx = "travis-kroot@idexx.com"
+var awesomeThoughts = "I am Travis and I am AWESOME";
+console.log(awesomeThoughts);
+var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
+console.log(funThoughts);
+$('#main').append(funThoughts);
+var formattedName = HTMLheaderName.replace("%data%", name);
+$('#header').append(formattedName);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$('#header').append(formattedRole);
